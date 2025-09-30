391123177d88578cde144432e9a3db554bafaa20
yyuhontop <dustinohlemann@gmail.com>
2025-09-30 10:14:21 +0200

    Added Copyright

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -2,15 +2,20 @@
 </script>
 
 <template>
-    <footer class="border-t border-gray-700/50 text-white mt-10 ">
+    <footer class="border-t border-gray-700/50 text-white mt-10">
         <div
-            class="max-w-[1950px] mx-auto flex flex-col md:flex-row justify-between items-start md:items-center p-3 gap-5">
+            class="max-w-[1950px] mx-auto flex flex-col md:flex-row justify-between items-start md:items-center p-3 gap-5 relative">
             <div class="flex flex-col gap-2">
                 <div class="flex flex-col">
                     <p class="text-lg font-bold">Revived.club</p>
                     <p class="text-gray-400 text-sm">Next Generation PVP Practice</p>
                 </div>
             </div>
+
+            <div class="absolute left-1/2 bottom-1 transform -translate-x-1/2 text-gray-400 text-sm">
+                <p>{{ new Date().getFullYear() }} © Revived.club</p>
+            </div>
+
 
             <div class="flex flex-row gap-2">
                 <router-link to="/"
