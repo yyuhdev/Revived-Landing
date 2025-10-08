b9578bcae55c989f94241ce9a9148d4a59be29d1
Dustin <160026484+yyuhdev@users.noreply.github.com>
2025-10-08 11:54:46 +0200

    implemented discord webhooks for applications

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\StaffApplication;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApplicationController extends Controller
-{   
+{
     public function saveApplication(Request $request): void
     {
         $request->validate([
@@ -18,6 +19,7 @@ class ApplicationController extends Controller
             'activity' => 'required',
         ]);
 
+
         StaffApplication::create([
             'ign' => $request->ign,
             'discord_tag' => $request->discord_tag,
@@ -26,5 +28,23 @@ class ApplicationController extends Controller
             'experience' => $request->experience,
             'activity' => $request->activity
         ]);
+
+        $webhook = config('discord.webhook');
+
+        Http::post($webhook, [
+            'embeds' => [
+                [
+                    'title' => "A new application has been submitted!",
+                    'description' =>
+                        "**IGN**: " . $request->ign .
+                        "\n **Discord Tag**: " . $request->discord_tag .
+                        "\n **Age**: " . $request->age .
+                        "\n **Reason**: " . $request->reason .
+                        "\n **Experience**: " . $request->experience .
+                        "\n **Activity**: " . $request->activity,
+                    'color' => '7506394',
+                ]
+            ]]);
+
     }
 }
---
+++ config/discord.php
@@ -0,0 +1,6 @@
+<?php
+
+
+return [
+    'webhook' => env('DISCORD_ALERT_WEBHOOK', 'default'),
+];
